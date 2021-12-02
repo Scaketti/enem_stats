bbf133e248a9b9a62fa2f398d126f6fa9a796a13
Matheus Scaketti <mts.scaketti@gmail.com>
2021-12-02 10:54:17 -0300

    merging codes

--- projeto.R
+++ projeto.R
@@ -6,19 +6,21 @@ library(pheatmap)
 #Read all datasets
 df <- read.csv(file="dataset.csv", sep = ";")
 
-cor_pele <- c('NÃ£o declarado','Branca',
+cor_pele <- c('Não declarado','Branca',
               'Preta','Parda','Amarela',
-              'IndÃ­gena','NÃ£o dispÃµe da informaÃ§Ã£o')
-escola <- c('NÃ£o respondeu', 'PÃºblica', 'Privada')
+              'Indígena','Não dispõe da informação')
+escola <- c('Não respondeu', 'Pública', 'Privada')
 
 df$TP_COR_RACA <- as.factor(df$TP_COR_RACA)
 levels(df$TP_COR_RACA) <- cor_pele
 df$TP_ESCOLA <- as.factor(df$TP_ESCOLA)
 levels(df$TP_ESCOLA) <- escola
 
+a <- df %>% filter(TP_ESCOLA != 'Não respondeu')
+
 row.names(df) <- paste0("row_", seq(nrow(df)))
 
-###### 1 - EstatÃ­sticas bÃ¡sicas ######
+###### 1 - Estatísticas básicas ######
 table(df$TP_SEXO, df$TP_COR_RACA)
 
 notas <- df[c('NU_NOTA_CH','NU_NOTA_MT','NU_NOTA_CN','NU_NOTA_LC','NU_NOTA_REDACAO')]
@@ -36,6 +38,8 @@ boxplot(notas$NU_NOTA_CN, notas$NU_NOTA_CH,
         notas$NU_NOTA_LC, notas$NU_NOTA_MT, 
         notas$NU_NOTA_REDACAO)
 
+summary(df)
+
 ###### 2 - Filtragens ######
 
 
@@ -44,23 +48,44 @@ boxplot(notas$NU_NOTA_CN, notas$NU_NOTA_CH,
 
 
 
-###### 4 - GrÃ¡ficos ######
+###### 4 - Gráficos ######
 
+df2 <- df %>%
+  group_by(TP_COR_RACA, TP_SEXO) %>%
+  summarise(
+    n = n(),
+    media_CN = mean(NU_NOTA_CN),
+    media_CH = mean(NU_NOTA_CH),
+    media_LC = mean(NU_NOTA_LC),
+    media_MT = mean(NU_NOTA_MT),
+    media_redacao = mean(NU_NOTA_REDACAO)
+    
+  )
 
+# Boxplot média das notas por escola
+# Boxplot média das notas por etnia
+# Corrplot?
+# ?
 
-###### 5 - Testes estatÃ­sticos ######
+###### 5 - Testes estatísticos ######
 
-
+#Comparar notas pelos anos
+#
 
 ###### 6 - PCA ######
 
-notas.pca <- prcomp(notas[, c(3:7)], 
+notas <- a[c('NU_NOTA_CH','NU_NOTA_MT','NU_NOTA_CN','NU_NOTA_LC','NU_NOTA_REDACAO')]
+notas$NU_NOTA_CH <- notas$NU_NOTA_CH/10
+notas$NU_NOTA_MT <- notas$NU_NOTA_MT/10
+notas$NU_NOTA_CN <- notas$NU_NOTA_CN/10
+notas$NU_NOTA_LC <- notas$NU_NOTA_LC/10
+
+notas.pca <- prcomp(notas, 
        center = TRUE,
        scale. = TRUE)
 
-
 fviz_pca_biplot(notas.pca, 
-                col.ind = df$TP_ESCOLA, palette = "jco", 
+                col.ind = as.factor(a$TP_ESCOLA), palette = "jco", 
                 addEllipses = TRUE, label = "var",
                 col.var = "black", repel = TRUE,
                 legend.title = "Tipo Escola"
@@ -92,8 +117,3 @@ row.names(ordered) <- row.names(grupos_k)
 pheatmap(ordered, cutree_rows = 6,scale = "column",
          cluster_rows = FALSE, cluster_cols = FALSE, 
          annotation_row = grupos_k, annotation_colors = cores, annotation_legend = TRUE)
-
-
-###### 10 - EXTRA ######
-
-
