f3c65c31fd0bfd7ea94506f446d4a9e40f8a63ba
joaorabel <95389169+joaorabel@users.noreply.github.com>
2021-12-02 01:03:50 -0300

    Update projeto.R

--- projeto.R
+++ projeto.R
@@ -6,19 +6,25 @@ library(pheatmap)
 #Read all datasets
 df <- read.csv(file="dataset.csv", sep = ";")
 
-cor_pele <- c('Não declarado','Branca',
+cor_pele <- c('NÃ£o declarado','Branca',
               'Preta','Parda','Amarela',
-              'Indígena','Não dispõe da informação')
-escola <- c('Não respondeu', 'Pública', 'Privada')
+              'IndÃ­gena','NÃ£o dispÃµe da informaÃ§Ã£o')
+escola <- c('NÃ£o respondeu', 'PÃºblica', 'Privada')
 
 df$TP_COR_RACA <- as.factor(df$TP_COR_RACA)
 levels(df$TP_COR_RACA) <- cor_pele
 df$TP_ESCOLA <- as.factor(df$TP_ESCOLA)
 levels(df$TP_ESCOLA) <- escola
+df$Q001 <- as.factor(df$Q001)
+df$Q002 <- as.factor(df$Q002)
+df$Q005 <- as.factor(df$Q005)
+df$Q006 <- as.factor(df$Q006)
+
 
 row.names(df) <- paste0("row_", seq(nrow(df)))
 
-###### 1 - Estatísticas básicas ######
+###### 1 - EstatÃ­sticas bÃ¡sicas ######
+summary(df)
 table(df$TP_SEXO, df$TP_COR_RACA)
 
 notas <- df[c('NU_NOTA_CH','NU_NOTA_MT','NU_NOTA_CN','NU_NOTA_LC','NU_NOTA_REDACAO')]
@@ -38,17 +44,72 @@ boxplot(notas$NU_NOTA_CN, notas$NU_NOTA_CH,
 
 ###### 2 - Filtragens ######
 
+df2 <- df_na %>%
+  group_by(TP_SEXO, TP_COR_RACA) %>%
+  summarise(
+    n = n(),
+    media_CN = mean(NU_NOTA_CN),
+    media_CH = mean(NU_NOTA_CH),
+    media_LC = mean(NU_NOTA_LC),
+    media_MT = mean(NU_NOTA_MT),
+    media_redacao = mean(NU_NOTA_REDACAO)
+  )
+
+df2 <- df_na %>%
+  group_by(TP_SEXO, TP_COR_RACA) %>%
+  summarise(
+    n = n(),
+    media_CN = mean(NU_NOTA_CN),
+    media_CH = mean(NU_NOTA_CH),
+    media_LC = mean(NU_NOTA_LC),
+    media_MT = mean(NU_NOTA_MT),
+    media_redacao = mean(NU_NOTA_REDACAO)
+  )
 
 
 ###### 3 - Limpeza de dados ######
+#veriricar se existem NAs
+is.na(df)
+table(is.na(df)) # tabela de frequencia 
+
+# retirar na
+df_na <- na.omit(df)
+
+#Detectando outlier 
+outlier_values_1 <- boxplot.stats(df_na$NU_NOTA_REDACAO)$out
+outlier_values_2 <- boxplot.stats(df_na$NU_NOTA_CN)$out
+outlier_values_3 <- boxplot.stats(df_na$NU_NOTA_CH)$out
+outlier_values_4 <- boxplot.stats(df_na$NU_NOTA_LC)$out
+outlier_values_5 <- boxplot.stats(df_na$NU_NOTA_MT)$out
+
+#verifica
+library(EnvStats)
+test <- rosnerTest(df_na$NU_NOTA_REDACAO,k=length(outlier_values_1),alpha = 0.05)
+test <- rosnerTest(df_na$NU_NOTA_CN,k=length(outlier_values_2),alpha = 0.05)
+test <- rosnerTest(df_na$NU_NOTA_CH,k=length(outlier_values_3),alpha = 0.05)
+test <- rosnerTest(df_na$NU_NOTA_LC,k=length(outlier_values_4),alpha = 0.05)
+test <- rosnerTest(df_na$NU_NOTA_MT,k=length(outlier_values_5),alpha = 0.05)
 
 
+#retira
+df_clean_1 <- df_na[-which(df_na$NU_NOTA_REDACAO %in% outlier_values_1),]
+df_clean_2 <- df_na[-which(df_na$NU_NOTA_CN %in% outlier_values_2),]
+df_clean_3 <- df_na[-which(df_na$NU_NOTA_CH %in% outlier_values_3),]
+df_clean_4 <- df_na[-which(df_na$df_na$NU_NOTA_Lc %in% outlier_values_4),]
+df_clean_5 <- df_na[-which(df_na$NU_NOTA_MT %in% outlier_values_5),]
 
-###### 4 - Gráficos ######
+###### 4 - GrÃ¡ficos ######
+
+ggplot(data = df_clean, aes(x=df_clean$TP_COR_RACA, y=df_clean$NU_NOTA_REDACAO,fill=df_clean$TP_SEXO)) + 
+  geom_boxplot(outlier.shape = NA)+
+  xlab("Etnia")+ylab("Média redacao")
+
+ggplot(data = df_clean, aes(x=df_clean$TP_COR_RACA, y=df_clean$NU_NOTA_REDACAO,fill=df_clean$Q006)) + 
+  geom_boxplot(outlier.shape = NA)+
+  xlab("Etnia")+ylab("Média redacao")
 
 
-
-###### 5 - Testes estatísticos ######
+###### 5 - Testes estatÃ­sticos ######
 
 
 
